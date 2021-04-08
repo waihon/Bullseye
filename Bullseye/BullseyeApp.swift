8ec52b8b1b4fe58ce7bf3ed1b211fef4b1aa89f9
Waihon Yew <yewwaihon@gmail.com>
2021-04-08 22:17:37 +0800

    Initial commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Yew Wai Hon on 08/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
