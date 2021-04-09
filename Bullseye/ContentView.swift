40f30195846ea6256fa2a44e9e3a4ef423d7fd2c
Waihon Yew <yewwaihon@gmail.com>
2021-04-09 23:42:22 +0800

    Bind the slider value to a state variable

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
 
   @State private var alertIsVisible: Bool = false
+  @State private var sliderValue: Double = 50.0
 
   var body: some View {
     VStack {
@@ -26,7 +27,7 @@ struct ContentView: View {
       HStack {
         Text("1")
           .bold()
-        Slider(value: .constant(50), in: 1.0...100.0)
+        Slider(value: self.$sliderValue, in: 1.0...100.0)
         Text("100")
           .bold()
       }
