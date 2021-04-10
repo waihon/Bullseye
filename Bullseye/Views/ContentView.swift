655deb2d9e96dacbf14e8b0b8cd42becdc7fab94
Waihon Yew <yewwaihon@gmail.com>
2021-04-10 16:44:05 +0800

    Bind game state to a new Game and replace hardcoded target with game's target

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
 
   @State private var alertIsVisible: Bool = false
   @State private var sliderValue: Double = 50.0
+  @State private var game: Game = Game()
 
   var body: some View {
     VStack {
@@ -20,7 +21,7 @@ struct ContentView: View {
         .multilineTextAlignment(.center)
         .lineSpacing(4.0)
         .font(.footnote)
-      Text("89")
+      Text(String(game.target))
         .kerning(-1.0)
         .font(.largeTitle)
         .fontWeight(.black)
