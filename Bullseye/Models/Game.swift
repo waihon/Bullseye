08b78394c12d17d99ac38eceba2f9a2284161b97
Waihon Yew <yewwaihon@gmail.com>
2021-04-12 22:43:48 +0800

    Simplify Game struct using type inference

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 struct Game {
-  var target: Int = Int.random(in: 1...100)
-  var score: Int = 0
-  var round: Int = 1
+  var target = Int.random(in: 1...100)
+  var score = 0
+  var round = 1
   
   func points(sliderValue: Int) -> Int {
-    let difference: Int = abs(sliderValue - self.target)
-    let awardedPoints: Int = 100 - difference
+    let difference = abs(sliderValue - self.target)
+    let awardedPoints = 100 - difference
     return awardedPoints
   }
 }
