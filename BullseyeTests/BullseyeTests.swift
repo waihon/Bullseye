73408101ff08cf2b9599d775a1d88db8713d0de0
Waihon Yew <yewwaihon@gmail.com>
2021-04-12 18:14:02 +0800

    Add  2 unit tests: testScorePositive and testScoreNegative

---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,37 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Yew Wai Hon on 12/04/2021.
+//
+
+import XCTest
+@testable import Bullseye
+
+class BullseyeTests: XCTestCase {
+  
+  var game: Game!
+  
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    game = Game()
+  }
+  
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    game = nil
+  }
+  
+  func testScorePositive() {
+    var guess = game.target + 5
+    var score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 95)
+  }
+  
+  func testScoreNegative() {
+    var guess = game.target - 5
+    var score = game.points(sliderValue: guess)
+    XCTAssertEqual(score, 95)
+  }
+  
+}
